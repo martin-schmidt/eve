f8dd1689dfc69e9ef0fa31cf36254036a94fed2d
islador <luke.isla@gmail.com>
2014-07-03 17:34:10 -0700

    Added errors and error messages for error codes 220, 221, 222 and 904. Added error messages for 901, 902 and 903.

--- lib/eve/errors/miscellaneous_errors.rb
+++ lib/eve/errors/miscellaneous_errors.rb
@@ -7,13 +7,29 @@ module Eve
       # 900 - Beta in progress, access denied.
       class BetaInProgress < MiscellaneousError; end
       # 901 - Web site database temporarily disabled.
-      class SiteDatabaseDisabled < MiscellaneousError; end
+      class SiteDatabaseDisabled < MiscellaneousError
+        def message
+          return "901 - Web site database temporarily disabled."
+        end
+      end
       # 902 - EVE backend database temporarily disabled.
-      class BackendDatabaseDisabled < MiscellaneousError; end
+      class BackendDatabaseDisabled < MiscellaneousError
+        def message
+          return "902 - EVE backend database temporarily disabled."
+        end
+      end
       # 903 - Rate limited [#]: please obey all cachedUntil timers.
-      class RateLimited < MiscellaneousError; end
-      # 999 - User forced test error condition.
-      class TestErrorForced < MiscellaneousError; end
+      class RateLimited < MiscellaneousError
+        def message
+          return "903 - Rate limited [{0}]: please obey all cachedUntil timers."
+        end
+      end
+
+      class IPBanned < MiscellaneousError
+        def message
+          return "904 - Your IP address has been temporarily blocked because it is causing too many errors. See the cacheUntil timestamp for when it will be opened again. IPs that continually cause a lot of errors in the API will be permanently banned, please take measures to minimize problematic API calls from your application."
+        end
+      end
     end
   end
 end
